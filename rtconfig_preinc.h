0bab76314853fcb7a103afabf244abfab66aa152
JunQiLiu <jassyliu96@gmail.com>
2021-09-10 16:26:59 +0800

    fix : 修正芯片选型

--- rtconfig_preinc.h
+++ rtconfig_preinc.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: JunQiLiu
+ * @Date: 2021-09-10 16:13:29
+ * @LastEditTime: 2021-09-10 16:26:38
+ * @Description: 
+ * @FilePath: \stm32f401ccu6_rtthread\rtconfig_preinc.h
+ *  
+ */
 
 #ifndef RTCONFIG_PREINC_H__
 #define RTCONFIG_PREINC_H__
@@ -6,7 +14,7 @@
 /* RT-Thread pre-include file */
 
 #define HAVE_CCONFIG_H
-#define STM32F407xx
+#define STM32F401xC
 #define USE_HAL_DRIVER
 #define __RTTHREAD__
 
