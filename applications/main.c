d59006b6dafb1143a7f0f16dcab778f2b460a252
JassyL <jassyliu96@gmail.com>
2021-09-11 14:16:09 +0800

    feat : 启用看门狗

--- applications/main.c
+++ applications/main.c
@@ -1,7 +1,7 @@
 /*
  * @Author: JunQiLiu
  * @Date: 2021-09-07 12:47:01
- * @LastEditTime: 2021-09-11 12:21:29
+ * @LastEditTime: 2021-09-11 14:14:44
  * @Description: 
  * @FilePath: \stm32f401ccu6_rtthread\applications\main.c
  *  
@@ -26,12 +26,58 @@
 /* defined the LED0 pin: PB1 */
 #define LED0_PIN    GET_PIN(C, 13)
 
+#define WDT_DEVICE_NAME    "wdt"    /* 看门狗设备名称 */
+
+static rt_device_t wdg_dev;         /* 看门狗设备句柄 */
+
+static void idle_hook(void)
+{
+    /* 在空闲线程的回调函数里喂狗 */
+    rt_device_control(wdg_dev, RT_DEVICE_CTRL_WDT_KEEPALIVE, NULL);
+}
+
+
+static int wdtStart(void)
+{
+    rt_err_t ret = RT_EOK;
+    rt_uint32_t timeout = 1;        /* 溢出时间，单位：秒 */
+    char device_name[RT_NAME_MAX];
+
+    rt_strncpy(device_name, WDT_DEVICE_NAME, RT_NAME_MAX);
+    /* 根据设备名称查找看门狗设备，获取设备句柄 */
+    wdg_dev = rt_device_find(device_name);
+    if (!wdg_dev)
+    {
+        rt_kprintf("find %s failed!\n", device_name);
+        return RT_ERROR;
+    }
+
+    /* 设置看门狗溢出时间 */
+    ret = rt_device_control(wdg_dev, RT_DEVICE_CTRL_WDT_SET_TIMEOUT, &timeout);
+    if (ret != RT_EOK)
+    {
+        rt_kprintf("set %s timeout failed!\n", device_name);
+        return RT_ERROR;
+    }
+    /* 启动看门狗 */
+    ret = rt_device_control(wdg_dev, RT_DEVICE_CTRL_WDT_START, RT_NULL);
+    if (ret != RT_EOK)
+    {
+        rt_kprintf("start %s failed!\n", device_name);
+        return -RT_ERROR;
+    }
+    /* 设置空闲线程回调函数 */
+    rt_thread_idle_sethook(idle_hook);
+
+    return ret;
+}
+
 int main(void)
 {
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-
+    wdtStart();
     modbusSlaveAppStart();
 
     rt_thread_t adc_tid;
